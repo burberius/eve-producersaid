4e4d8c755b5f42fa08a117ba5ba0269a99e449b3
Jens Oberender <j.obi@troja.net>
2015-04-23 20:33:04 +0200

    Formated pom file and added proper copyright notice via maven.

--- src/main/java/net/troja/eve/producersaid/data/BlueprintProduction.java
+++ src/main/java/net/troja/eve/producersaid/data/BlueprintProduction.java
@@ -1,23 +1,26 @@
-/*******************************************************************************
- * Copyright (c) 2015 Jens Oberender <j.obi@troja.net>
- *
- * This file is part of Eve Producer's Aid.
- *
- * Eve Producer's Aid is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
+package net.troja.eve.producersaid.data;
+
+/*
+ * ========================================================================
+ * Eve Producer's Aid
+ * ------------------------------------------------------------------------
+ * Copyright (C) 2014 - 2015 Jens Oberender <j.obi@troja.net>
+ * ------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
- *******************************************************************************/
-package net.troja.eve.producersaid.data;
+ * ========================================================================
+ */
 
 import net.troja.eve.producersaid.utils.EveFormatter;
 
